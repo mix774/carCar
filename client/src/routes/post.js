f4da15eb9b77982781e5a43c4f9e162060eb5d71
mix774 <nikitami2607@gmail.com>
2022-06-16 10:03:54 +0300

    Добавлена страница объявления

--- client/src/routes/post.js
+++ client/src/routes/post.js
@@ -29,36 +29,36 @@ export default function Post() {
 			<Container
 				className={classes.header}
 			>
-				<CardMedia
+				{post.images && <CardMedia
 					style={{ objectFit: "contain", cursor: "pointer" }}
 					className={classes.image}
 					component="img"
 					width="560"
 					height="320"
 					image={`http://localhost:3000/image/${post.images[0]}`}
-				/>
+				/>}
 			</Container>
 			<Container className={classes.description}>
 				<Typography
-				component="div"
-				style={{margin: "15px 0 15px 0", fontSize: "18px"}}
-				className={classes.descriptionHeader}
+					component="div"
+					style={{ margin: "15px 0 15px 0", fontSize: "18px" }}
+					className={classes.descriptionHeader}
 				>
 					Описание
 				</Typography>
-				<p className={classes.amout} style={{backgroundColor: "#d7d7d7", fontSize: "18px", padding: "10px"}}>
+				<p className={classes.amout} style={{ backgroundColor: "#d7d7d7", fontSize: "18px", padding: "10px" }}>
 					Количество владельцев: {post.amountOfOwners}
 				</p>
-				<p className={classes.year} style={{ fontSize: "18px", padding: "5px", marginTop: "0"}}>
+				<p className={classes.year} style={{ fontSize: "18px", padding: "5px", marginTop: "0" }}>
 					Год выпуска: {post.year}
 				</p>
-				<p className={classes.price} style={{ backgroundColor: "#d7d7d7", fontSize: "18px", padding: "10px"}}>
+				<p className={classes.price} style={{ backgroundColor: "#d7d7d7", fontSize: "18px", padding: "10px" }}>
 					Цена: {post.price}
 				</p>
-				<p className={classes.mileage} style={{ fontSize: "18px", padding: "10px"}}>
+				<p className={classes.mileage} style={{ fontSize: "18px", padding: "10px" }}>
 					Пробег: {post.mileage}
 				</p>
-				<p className={classes.description} style={{ backgroundColor: "#d7d7d7", fontSize: "18px", padding: "10px"}}>
+				<p className={classes.description} style={{ backgroundColor: "#d7d7d7", fontSize: "18px", padding: "10px" }}>
 					Описание: {post.description}
 				</p>
 			</Container>
